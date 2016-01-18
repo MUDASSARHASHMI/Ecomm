87ca88b9604cf3dc5f7182d63f48ec13a6c97692
Mudassar Hashmi <mudassarziahashmim@hotmail.com>
2016-01-17 22:19:42 -0700

    Blog Application to commit
    Commit blog application with complete package

---
+++ ecommerce/blog/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+
+def blog_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, 'blog/post_form.html', context)
+
+def blog_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "My User List"
+    }
+    return render(request, 'blog/list.html', context)
+
+def blog_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+    return render(request, 'blog/detail.html', context)
+
+
+
+def blog_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form
+    }
+    return render(request, 'blog/post_form.html', context)
+
+
+def blog_delete(request):
+    return HttpResponse ("<h1>Update</h1>")
