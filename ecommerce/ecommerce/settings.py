5831d548ade819bf3510e37d88234ffdade141e9
Mudassar Hashmi <mudassarziahashmim@hotmail.com>
2016-01-24 05:59:00 -0700

    Removed Debug Toolbar
    Will add debug after project is completed

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'registration',
     'blog',
 
+
 ]
 
 MIDDLEWARE_CLASSES = [
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,15 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls', namespace='blog')),# Name space makes your urls unique even if they are of same name in multiple apps
-    #url(r'^__debug__/', include(debug_toolbar.urls)),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += patterns('',
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
