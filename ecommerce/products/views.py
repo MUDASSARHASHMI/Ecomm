41703bf6bc5c91a03ce7d02bbb3270d417229728
Mudassar Hashmi <mudassarziahashmim@hotmail.com>
2016-03-24 04:32:27 -0700

    Adding Products Mixins

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404, Http404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, Http404, redirect
 from django.db.models import Q
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -11,7 +12,7 @@ class VariationListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(VariationListView, self).get_context_data(*args, **kwargs)
-        context["formset"] = VariationInventoryFormSet()
+        context["formset"] = VariationInventoryFormSet(queryset=self.get_queryset())
         return context
 
     def get_queryset(self, *args, **kwargs):
@@ -23,7 +24,18 @@ class VariationListView(ListView):
         return queryset
 
     def post(self, request, *args, **kwargs):
-        print request.POST
+        formset = VariationInventoryFormSet(request.POST, request.FILES)
+
+        if formset.is_valid():
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                product_pk = self.kwargs.get("pk")
+                product = get_object_or_404(Product, pk=product_pk)
+                new_item.product = product
+                new_item.save()
+            messages.success(request, 'Form Updated')
+            return redirect('products')
         raise Http404
 
 
